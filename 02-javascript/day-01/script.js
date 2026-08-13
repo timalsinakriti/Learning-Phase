0af8530dde1d435a8adaca8199dadc4ac5fe9919
timalsinakriti <timalsinakriti763@gmail.com>
2026-08-13 15:18:01 +0545

    Start learning web development

---
+++ 02-javascript/day-01/script.js
@@ -0,0 +1,22 @@
+//console.log("My JavaScript is working!");
+//Basic LET and CONST
+
+
+/*let name = "Kriti";
+let age = 21;
+const country = "Nepal";
+let goal = "Frontend Developer";
+let university = "Patan Multiple Campus";
+console.log(name);
+console.log(age);
+console.log(country);
+console.log(goal);
+console.log(university);*/
+
+//Numbers & Calculations
+
+/*const price = 500;
+const quantity = 3;
+const total = price * quantity;
+console.log(total);*/
+
